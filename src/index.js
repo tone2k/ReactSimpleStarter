55738cf962eb80dded44f7e9ee80a5583fce12c5
Tony Thompson <penguinprofession@icloud.com>
2018-06-18 12:09:11 -0400

    imported youtube search package and accessed API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 //import React
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyCrVDceP1-KwRsIVi12ODPCwS2oSHe-_7k';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
+    console.log(data);
+});
 
 //A. Create Component that produces HTML
 
